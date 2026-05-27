0af67ada5e6e0fa4a37b0fe5f4fff202353fa837
Yash Baghel <yashbaghel@Yashs-MacBook-Air.local>
2026-05-27 22:46:34 +0530

    implement pub sub broadcasting for commentary

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import express from "express"
-import http from 'http'
+import express from "express";
+import http from 'http';
+import cors from 'cors'
 
 import { matchRouter } from "./routes/matches.js";
-import { attatchWebsocket } from "./ws/server.js";
+import { attachWebSocketServer } from "./ws/server.js";
 import { securityMiddleware } from "./arcjet.js";
+import { commentaryRouter } from "./routes/commantry.js";
 
 const app = express();
 const PORT = Number(process.env.PORT || 8000);
@@ -13,15 +15,26 @@ const server = http.createServer(app);
 
 app.use(express.json());
 
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow only requests from this origin
+    methods: 'GET,POST', // Allow only these methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow only these headers
+};
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
     res.send('Hello from express server!');
 });
 
 app.use(securityMiddleware())
 
+app.use('/matches/:id/commentary', commentaryRouter);
 app.use('/matches', matchRouter);
-const { broadcastMatchCreated } = attatchWebsocket(server);
+
+const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary = broadcastCommentary;
 
 server.listen(PORT, HOST, () => {
     const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`
